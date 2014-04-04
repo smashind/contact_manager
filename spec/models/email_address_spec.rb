2ec9650fe06c04e3c42706f4957638e01d735537
Danny Baumann <danthemanvt@gmail.com>
2014-04-04 04:41:41 -0700

    Add validation test to check that email address is associate with person id

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe EmailAddress do
   let(:email_address) do
-  	EmailAddress.new(address: "testemail@testing.com")
+  	EmailAddress.new(address: "testemail@testing.com", person_id: 1)
   end
 
   it 'is valid' do
@@ -11,6 +11,11 @@ describe EmailAddress do
 
   it 'is invalid without an address' do
   	email_address.address = nil
+  	expect(email_address).to_not be_valid
+  end
+
+  it 'is invalid without a person id' do
+  	email_address.person_id = nil
   	expect(email_address).to_not be_valid
   end
 
